862d586c82d1414bb3dfc1dc6717429c14171017
roomee1-admin <jaredbgoddard@gmail.com>
2023-02-16 16:24:40 -0800

    WorldGeneration Rough Draft

---
+++ DeliveryGame/Assets/Scripts/World Generation/ChunkData.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Chunk {
+    public ChunkData chunkData;
+
+    public GameObject parent;
+    public List<IntersectionBehavior> intersectionBehaviors = new List<IntersectionBehavior>();
+    public List<RoadBehavior> roadBehaviors = new List<RoadBehavior>();
+    public List<RoadBehavior>[] externalRoadBehavior = new List<RoadBehavior>[8];
+
+    public List<BuildingBehavior> buildingBehavior = new List<BuildingBehavior>();
+    public List<BuildingBehavior>[] externalBuildingBehavior = new List<BuildingBehavior>[8];
+
+    public Chunk() {
+        for (int i = 0; i < 8; i++) {
+            externalRoadBehavior[i] = new List<RoadBehavior>();
+        }
+        for (int i = 0; i < 8; i++) {
+            externalBuildingBehavior[i] = new List<BuildingBehavior>();
+        }
+    }
+
+    public void delete() {
+        foreach(BuildingBehavior b in buildingBehavior) {
+            MonoBehaviour.Destroy(parent);
+        }
+    }
+}
+
+public class ChunkData {
+    public Vector2Int chunkPos { get; private set; } // the center of the chunk is at chunkPos * chunkSize.
+
+    public List<Vector2> intersections { get; private set; } // list of intersections in world units
+    public List<RoadInfo> roads { get; private set; } // list of roads. roads are straight lines between two intersections
+    public List<BuildingInfo> buildings { get; private set; } //list of buildings
+
+    public List<RoadInfo>[] externalRoads { get; private set; }// list of roads going outside of the chunk in world units. roads are straight lines between two intersections
+    public List<BuildingInfo>[] externalBuildings { get; private set; } //list of buildings on the border of a chunk
+
+
+    public ChunkData(
+        Vector2Int chunkPos, 
+        List<Vector2> intersections, 
+        List<RoadInfo> roads, 
+        List<RoadInfo>[] externalRoads,
+        List<BuildingInfo> buildings,
+        List<BuildingInfo>[] externalBuildings
+
+    ) {
+        this.chunkPos = chunkPos;
+        this.intersections = intersections;
+        this.roads = roads;
+        this.externalRoads = externalRoads;
+        this.buildings = buildings;
+        this.externalBuildings = externalBuildings;
+    }
+
+}
+
+public class RoadInfo {
+    public Vector2 a { get; private set; }
+    public Vector2 b { get; private set; }
+
+    public RoadInfo(Vector2 a, Vector2 b) {
+        this.a = a;
+        this.b = b;
+    }
+}
+
+public class BuildingInfo {
+    public Vector2 pos;
+    public int buildingType;
+}
+
---
+++ DeliveryGame/Assets/Scripts/World Generation/WorldGenerationConstants.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = System.Random;
+
+public class WorldGenerationConstants
+{
+    public static int chunkSize = 128; // width and height of a chunk in world units. multiple of roadLength.
+    public static float roadLength = 16; // distance between the center of 2 intersections in world units
+    public static float roadWidth = 2; // width of a road in world units
+
+    public static int buildingsPerRoad = 2;
+    public static float buildingWidth = (roadLength - roadWidth) / buildingsPerRoad; // width (and length) of a building in world units
+    public static int chunkBuildingWidth = (chunkSize / (int)roadLength) * buildingsPerRoad;
+    public static int buildingPermSize = chunkBuildingWidth + 1;
+}
+
+public class Math {
+
+    public static int mod(int n, int m) {
+        return ((n % m) + m) % m;
+    }
+
+    public static int[][] generatePerm(int seedX, int seedY, int size) {
+        int[][] perm = new int[size][];
+
+        Random rX = new Random(seedX);
+        Random rY = new Random(seedY);
+        for (int i = 0; i < size; i++) {
+            perm[i] = new int[size];
+
+            for(int j = 0; j < size; j++) {
+                perm[i][j] = i * size + j;
+            }
+        }
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size; j++) {
+                int x = mod((seedX + rX.Next(0, size)), size);
+                int y = mod((seedY + rY.Next(0, size)), size);
+                int temp = perm[i][j];
+                perm[i][j] = perm[x][y];
+                perm[x][y] = temp;
+            }
+        }
+        return perm;
+    }
+}
