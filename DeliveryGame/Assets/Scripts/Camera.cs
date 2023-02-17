2e032472b3c058866aadd1e66bd1614307cbbafe
Chris-Lasneski <lasneskc@engr.orst.edu>
2023-02-17 15:33:59 -0800

    updated camera script, a bit glitchy when moving around need to figure out how to adjust range to prevent y axis looping through the ground and player

---
+++ DeliveryGame/Assets/Scripts/Camera.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    //public Transform player;
+    //public float pLerp = 0.02f;
+    //public float rLerp = 0.01f;
+
+    public GameObject player;
+    public GameObject cameraPivot;
+    public float yOffset = 1f;
+    public float sensitivity = 30f;
+    public Camera cam;
+
+    public float scrollSens = 5f;
+    public float scrollDamp = 6f;
+
+    public float zoomMin = 3.5f;
+    public float zoomMax = 15f;
+    public float zoomDefault = 10f;
+    public float zoomDistance;
+
+    public float collisionSensitivity = 4.5f;
+
+    private RaycastHit _camHit;
+    private Vector3 _camDist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camDist = cam.transform.localPosition;
+        zoomDistance = zoomDefault;
+        _camDist.z = zoomDistance;
+
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        cameraPivot.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yOffset, player.transform.position.z);
+
+        var rotation = Quaternion.Euler(cameraPivot.transform.rotation.eulerAngles.x - Input.GetAxis("Mouse Y") * sensitivity / 2,
+            cameraPivot.transform.rotation.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivity,
+            cameraPivot.transform.rotation.eulerAngles.z);
+
+        cameraPivot.transform.rotation = rotation;
+
+        if(Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            var scrollAmount = Input.GetAxis("Mouse ScrollWhell") * scrollSens;
+            scrollAmount *= zoomDistance * 0.3f;
+            zoomDistance += -scrollAmount;
+            zoomDistance = Mathf.Clamp(zoomDistance, zoomMin, zoomMax);
+        }
+
+        if(_camDist.z != -zoomDistance)
+        {
+            _camDist.z = Mathf.Lerp(_camDist.z, -zoomDistance, Time.deltaTime * scrollDamp);
+        }
+
+        cam.transform.localPosition = _camDist;
+
+        GameObject obj = new GameObject();
+        obj.transform.SetParent(cam.transform.parent);
+        obj.transform.localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, cam.transform.localPosition.z - collisionSensitivity);
+
+        if(Physics.Linecast(cameraPivot.transform.position, obj.transform.position, out _camHit))
+        {
+            cam.transform.position = _camHit.point;
+
+            var localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y,
+                cam.transform.localPosition.z + collisionSensitivity);
+            cam.transform.localPosition = localPosition;
+        }
+        Destroy(obj);
+
+        if (cam.transform.localPosition.z > -1f)
+        {
+            cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, -1f);
+        }
+
+        //transform.position = Vector3.Lerp(transform.position, player.position, pLerp);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, pLerp);
+    }
+}
--- DeliveryGame/Assets/Scripts/CameraFollow.cs
+++ DeliveryGame/Assets/Scripts/CameraFollow.cs
@@ -6,15 +6,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float smoothing;
-    public float rotSmoothing;
-    public Transform player;
-
-    public float mouseSens = 1.0f;
-    public float clampAngle = 80.0f;
-
-    //For this Version used Cyber Chroma's camera follow script:https://www.youtube.com/watch?v=Ul01SxwPIvk
-    // No idea if we'll keep this follow script or come back and change it later on.
+    public float sens = 0.5f;
+    public Vector2 turn;
 
     // Start is called before the first frame update
     void Start()
@@ -25,16 +18,15 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        {
-            transform.position = Vector3.Lerp(transform.position, player.position, smoothing);
-            transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, rotSmoothing);
-            transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
-        }
-        else
-        {
-            
+        turn.y += Input.GetAxis("Mouse Y") * sens;
+        turn.x += Input.GetAxis("Mouse X") * sens;
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
 
-        }
+        //if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        //{
+        //    transform.position = Vector3.Lerp(transform.position, player.position, smoothing);
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, rotSmoothing);
+        //    transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
+        //}
     }
 }
