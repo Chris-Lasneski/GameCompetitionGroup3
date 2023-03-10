94c77ed6523c28a94cadd40d0a9e3bb2c70774fe
Chris-Lasneski <lasneskc@engr.orst.edu>
2023-03-09 17:17:49 -0800

    pushing everything to repo for beta build

--- DeliveryGame/Assets/Scripts/Camera.cs
+++ DeliveryGame/Assets/Scripts/Camera.cs
@@ -11,14 +11,14 @@ public class Camera : MonoBehaviour
     public GameObject player;
     public GameObject cameraPivot;
     public float yOffset = 1f;
-    public float sensitivity = 30f;
+    public float sensitivity = 10f;
     public Camera cam;
 
     public float scrollSens = 5f;
     public float scrollDamp = 6f;
 
-    public float zoomMin = 3.5f;
-    public float zoomMax = 15f;
+    public float zoomMin = 3f;
+    public float zoomMax = 25f;
     public float zoomDefault = 10f;
     public float zoomDistance;
 
@@ -26,6 +26,9 @@ public class Camera : MonoBehaviour
 
     private RaycastHit _camHit;
     private Vector3 _camDist;
+
+    private bool _enabled = true;
+    private bool paused = false;
 
     // Start is called before the first frame update
     void Start()
@@ -38,51 +41,60 @@ public class Camera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        cameraPivot.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yOffset, player.transform.position.z);
-
-        var rotation = Quaternion.Euler(cameraPivot.transform.rotation.eulerAngles.x - Input.GetAxis("Mouse Y") * sensitivity / 2,
-            cameraPivot.transform.rotation.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivity,
-            cameraPivot.transform.rotation.eulerAngles.z);
-
-        cameraPivot.transform.rotation = rotation;
-
-        if(Input.GetAxis("Mouse ScrollWheel") != 0f)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            var scrollAmount = Input.GetAxis("Mouse ScrollWhell") * scrollSens;
-            scrollAmount *= zoomDistance * 0.3f;
-            zoomDistance += -scrollAmount;
-            zoomDistance = Mathf.Clamp(zoomDistance, zoomMin, zoomMax);
+            _enabled = !_enabled;
+            paused = !paused;
+            Cursor.visible = paused;
         }
-
-        if(_camDist.z != -zoomDistance)
+        if (_enabled)
         {
-            _camDist.z = Mathf.Lerp(_camDist.z, -zoomDistance, Time.deltaTime * scrollDamp);
+            cameraPivot.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yOffset, player.transform.position.z);
+
+            var rotation = Quaternion.Euler(Mathf.Clamp(cameraPivot.transform.rotation.eulerAngles.x - Input.GetAxis("Mouse Y") * sensitivity / 2, 0, 80),
+                cameraPivot.transform.rotation.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivity,
+                cameraPivot.transform.rotation.eulerAngles.z);
+
+            cameraPivot.transform.rotation = rotation;
+
+            if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+            {
+                var scrollAmount = Input.GetAxis("Mouse ScrollWheel") * scrollSens;
+                scrollAmount *= zoomDistance * 0.3f;
+                zoomDistance += -scrollAmount;
+                zoomDistance = Mathf.Clamp(zoomDistance, zoomMin, zoomMax);
+            }
+
+            if (_camDist.z != -zoomDistance)
+            {
+                _camDist.z = Mathf.Lerp(_camDist.z, -zoomDistance, Time.deltaTime * scrollDamp);
+            }
+
+            cam.transform.localPosition = _camDist;
+
+            GameObject obj = new GameObject();
+            obj.transform.SetParent(cam.transform.parent);
+            obj.transform.localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, cam.transform.localPosition.z - collisionSensitivity);
+
+            if (Physics.Linecast(cameraPivot.transform.position, obj.transform.position, out _camHit))
+            {
+                cam.transform.position = _camHit.point;
+
+                var localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y,
+                    cam.transform.localPosition.z + collisionSensitivity);
+                cam.transform.localPosition = localPosition;
+            }
+            Destroy(obj);
+
+            if (cam.transform.localPosition.z > -1f)
+            {
+                cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, -1f);
+            }
+
+            //transform.position = Vector3.Lerp(transform.position, player.position, pLerp);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, pLerp);
         }
-
-        cam.transform.localPosition = _camDist;
-
-        GameObject obj = new GameObject();
-        obj.transform.SetParent(cam.transform.parent);
-        obj.transform.localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, cam.transform.localPosition.z - collisionSensitivity);
-
-        if(Physics.Linecast(cameraPivot.transform.position, obj.transform.position, out _camHit))
-        {
-            cam.transform.position = _camHit.point;
-
-            var localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y,
-                cam.transform.localPosition.z + collisionSensitivity);
-            cam.transform.localPosition = localPosition;
-        }
-        Destroy(obj);
-
-        if (cam.transform.localPosition.z > -1f)
-        {
-            cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, -1f);
-        }
-
-        //transform.position = Vector3.Lerp(transform.position, player.position, pLerp);
-        //transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, pLerp);
     }
 }
--- DeliveryGame/Assets/Scripts/CarController.cs
+++ DeliveryGame/Assets/Scripts/CarController.cs
@@ -1,79 +1,172 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.IO;
+using System;
 
 public class CarController : MonoBehaviour
 {
+    // colliders for the wheels, deals with brake torque and motor torque
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider backLeftWheelCollider;
+    [SerializeField] private WheelCollider backRightWheelCollider;
 
-    // IMPORTANT for first build
-    // For now the code is heavily based off Cyber Chroma's tutorial for creating a car controller in Unity: https://www.youtube.com/watch?v=Ul01SxwPIvk
-    // I plan to come back and heavily modify it later when we're further along in development and more pieces, such as an acutal car with wheels is implemented to add colliders and implement more variables into the speed/acceleration calculations
+    // the wheel objects go into these, needed to make the wheels turn and spin
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform backLeftWheelTransform;
+    [SerializeField] private Transform backRightWheelTransform;
 
-    private float speed = 25.0f;
-    private float turnspeed = 25.0f;
-    private float velocity = 0.0f;
-    private float acceleration = 1.0f;
-    private float mass = 1.5f;
-    private float topSpeed = 25.0f;
-    private float reverseTopSpeed = 5.0f;
-    private float breakTorque = 1.0f;
+    public float acceleration;
+    public float maxSteeringAngle;
+    private float horizontalInput;
+    private float verticalInput;
+    private bool brakeing = false;
+    private bool ebrake = false;
+    private float steeringAngle;
+    public float brakeTorque;
+    public bool isFrontDrive;
+    private float maxSpeed = 36f;
+    // modifiers from upgrades, will change when we can draw from upgrade list.
+    private float maxSpeedModifier = 7f;
+    private float brakeTorqueModifier = 7500f;
+    // will ned to look into wheel traction, currently doesn't make the car accelrate much faster as much as making the wheels spin faster in a burnout
+    private float accelerationModifier = 400f;
 
-    private Rigidbody rb;
+    private Vector3 velocity;
+    private Vector3 localVel;
 
-    // Start is called before the first frame update
-    void Start()
+    // use to make center of mass to help prevent car from flipping when turning, id recommend only adjusting the y value in the inspector
+    // x and z will move it away from middle of car to front back/side to side. lower will do better at keepin it on the "road"
+    // cars rigidbody
+    public Rigidbody rb;
+    // center of mass vector
+    public Vector3 com;  
+
+    private void Start()
     {
+        // get the rigidbody of the car and set the center of mass to the center of mass vector. Helps car not tip over.
         rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = com;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public void FixedUpdate()
     {
-        Move();
-        Turn();
+        // in functions to not clutter up main loop
+        GetInput();
+        HandleMotorPower();
+        Steering();
+        UpdateWheels();
     }
 
-    void Move() {
-        // forward and backward
-        if(Input.GetKey(KeyCode.W)) {
-            if (velocity < topSpeed)
+    // seems self explanitory
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        ebrake = Input.GetKey(KeyCode.Space);
+    }
+
+    // handles the total power of the motor, do all motor power calcs here. assuming front wheel drive
+    private void HandleMotorPower()
+    {
+        // check if we're at max speed + maxSpeedModifier, if were less add acceleration to the wheels, else keep it at max speed + modifier
+        if (rb.velocity.magnitude <= (maxSpeed + maxSpeedModifier))
+        {
+            // check if front or wheel wheel drive and apply acceleration to proper wheel set.
+            if (isFrontDrive)
             {
-                velocity += acceleration;
-            }
-            else if(velocity == topSpeed)
-            {
-                velocity += 0.0f;
+                frontLeftWheelCollider.motorTorque = verticalInput * (acceleration + accelerationModifier);
+                frontRightWheelCollider.motorTorque = verticalInput * (acceleration + accelerationModifier);
             }
             else
             {
-                velocity -= mass;
+                backLeftWheelCollider.motorTorque = verticalInput * (acceleration + accelerationModifier);
+                backRightWheelCollider.motorTorque = verticalInput * (acceleration + accelerationModifier);
             }
-            rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * velocity);
         }
-        else if(Input.GetKey(KeyCode.S)) {
-            if (velocity >= reverseTopSpeed)
+        else
+        {
+            if (isFrontDrive)
             {
-                velocity -= breakTorque;
+                frontLeftWheelCollider.motorTorque = verticalInput * (maxSpeed + maxSpeedModifier);
+                frontRightWheelCollider.motorTorque = verticalInput * (maxSpeed + maxSpeedModifier);
             }
-            rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * -speed);        
+            else
+            {
+                backLeftWheelCollider.motorTorque = verticalInput * (maxSpeed + maxSpeedModifier);
+                backRightWheelCollider.motorTorque = verticalInput * (maxSpeed + maxSpeedModifier);
+            }
         }
-        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
-        localVelocity.x = 0;
-        rb.velocity = transform.TransformDirection(localVelocity);
+
+        // handles the braking aspect of the car, probably should be moved to new function but fine for now.
+        brakeing = isBraking();
+        if(brakeing)
+        {
+            frontRightWheelCollider.brakeTorque = brakeTorque + brakeTorqueModifier;
+            frontLeftWheelCollider.brakeTorque = brakeTorque + brakeTorqueModifier;
+        }
+        else
+        {
+            frontRightWheelCollider.brakeTorque = 0f;
+            frontLeftWheelCollider.brakeTorque = 0f;
+        }
+        // using this as a full brake to engage brakes even when car is stopped, other brake will start moving in opposite direction when velocity is 0.
+        // have it set to back wheels as having it set to front wheel braking along with previous brake set causes 'S' braking to not brake correctly and will intermittenly apply the brakes
+        if (ebrake)
+        {
+            backLeftWheelCollider.brakeTorque = brakeTorque + brakeTorqueModifier;
+            backRightWheelCollider.brakeTorque = brakeTorque + brakeTorqueModifier;
+        }
+        else
+        {
+            backLeftWheelCollider.brakeTorque = 0f;
+            backRightWheelCollider.brakeTorque = 0f;
+        }
     }
 
-    void Turn() {
-        //Turning
-        if (velocity > 0.0f)
+    // checks to see if car is breaking by pressing reverse, then switch to reverse and check for forward input. doens't care about ebrake
+    private bool isBraking()
+    {
+        velocity = rb.velocity;
+        localVel = transform.InverseTransformDirection(velocity);
+
+        if(localVel.z > 0f && Input.GetKey(KeyCode.S))
         {
-            if (Input.GetKey(KeyCode.D))
-            {
-                rb.AddTorque(Vector3.up * turnspeed);
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                rb.AddTorque(-Vector3.up * turnspeed);
-            }
+            return true;
         }
+        else if(localVel.z < 0f && Input.GetKey(KeyCode.W))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // deals with the steering of the car, currently only supports front wheel steering cars. don't know if rear wheel steering would make a diffenrce at the moment
+    private void Steering()
+    {
+        steeringAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = steeringAngle;
+        frontRightWheelCollider.steerAngle = steeringAngle;
+    }
+
+    // helper function dealing with wheel updates, reduces clutter in main loop
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(backLeftWheelCollider, backLeftWheelTransform);
+        UpdateSingleWheel(backRightWheelCollider, backRightWheelTransform);
+    }
+
+    // updates a single wheel making them spin and move with car
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
     }
 }
