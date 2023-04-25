24b69e309dd806c4193a80dfcc7356063ef4edd4
Chris-Lasneski <lasneskc@engr.orst.edu>
2023-04-25 13:31:07 -0700

    try this again

--- DeliveryGame/Assets/Scripts/MenuUI.cs
+++ DeliveryGame/Assets/Scripts/MenuUI.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
     public GameObject menu;
     public bool paused = false;
+    public Button exitButton;
+    public Button resumeButton;
+    public Button settingsButton;
 
     // Start is called before the first frame update
     void Start()
     {
         menu.SetActive(false);
+
+
+        //Button btn = exitButton.GetComponent<Button>();
+        //btn.onClick.AddListener(ExitTaskOnClick);
+        //Button rbtn = resumeButton.GetComponent<Button>();
+        //rbtn.onClick.AddListener(ResumeButtonClick);
+        //Button sbtn = settingsButton.GetComponent<Button>();
+
     }
 
     // Update is called once per frame
@@ -19,16 +31,31 @@ public class MenuUI : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             paused = !paused;
-            if (paused)
-            {
-                menu.SetActive(true);
-                Time.timeScale = 0.0f;
-            } 
-            else
-            {
-                menu.SetActive(false);
-                Time.timeScale = 1.0f;
-            }
+            PauseHandler(paused);
         }
     }
+
+    public void PauseHandler( bool p)
+    {
+        if (paused)
+        {
+            menu.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            menu.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    //public void ExitTaskOnClick()
+    //{
+    //    Application.Quit();
+    //}
+
+    //public void ResumeButtonClick()
+    //{
+    //    PauseHandler(false);
+    //}
 }
