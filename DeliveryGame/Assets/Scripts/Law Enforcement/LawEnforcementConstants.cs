fd879311e2d4b3ab0f1f5b1c12842bcb4e3e7007
roomee1-admin <jaredbgoddard@gmail.com>
2023-05-11 19:26:27 -0700

    release 1.1

--- DeliveryGame/Assets/Scripts/Law Enforcement/LawEnforcementConstants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LawEnforcementConstants
-{
-    public static float CameraSpeedLimit = 50;
-    public static float CameraSpeedCost = 100;
-}
